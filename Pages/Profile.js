4a938dfe1b537abbdc81a04eae10541ee3a4e2c6
Harsh Kapadia <harshgkapadia@gmail.com>
2021-10-25 19:01:12 +0530

    :sparkles: feat: Populate User Profile page with DB data

--- Pages/Profile.js
+++ Pages/Profile.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { ScrollView } from "react-native";
 import { DataTable } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Profile = () => {
 	const navigation = useNavigation();
+
+	const [userData, setUserData] = useState({});
+
+	useEffect(() => {
+		fetchUserProfile();
+	}, []);
+
+	const fetchUserProfile = async () => {
+		const token = await AsyncStorage.getItem("hys_gems_auth_token");
+
+		const response = await fetch(
+			"https://hps-gems.herokuapp.com/server/api/get-user.php",
+			{
+				headers: {
+					"Accept": "application/json",
+					"Authentication": `Bearer ${token}`
+				}
+			}
+		);
+
+		const userProfileData = await response.json();
+
+		setUserData(userProfileData.data);
+	};
 
 	return (
 		<ScrollView>
@@ -15,33 +40,37 @@ const Profile = () => {
 					<DataTable style={styles.table}>
 						<DataTable.Row>
 							<DataTable.Cell>First name</DataTable.Cell>
-							<DataTable.Cell>Saikiran</DataTable.Cell>
+							<DataTable.Cell>
+								{userData.first_name ? userData.first_name : ""}
+							</DataTable.Cell>
 						</DataTable.Row>
 
 						<DataTable.Row>
 							<DataTable.Cell>Last Name</DataTable.Cell>
-							<DataTable.Cell>Jakkan</DataTable.Cell>
+							<DataTable.Cell>
+								{userData.last_name ? userData.last_name : ""}
+							</DataTable.Cell>
 						</DataTable.Row>
 
 						<DataTable.Row>
 							<DataTable.Cell>Address</DataTable.Cell>
 							<DataTable.Cell>
-								Anita Nagar, Bldg no.11, C wing, Flat no. 104,
-								Lokhandwala Complex, Akurli Road, Kandivali -
-								East
+								{userData.address ? userData.address : ""}
 							</DataTable.Cell>
 						</DataTable.Row>
 
 						<DataTable.Row>
 							<DataTable.Cell>e-mail</DataTable.Cell>
 							<DataTable.Cell>
-								saikiran.jakkan@gmail.com
+								{userData.email ? userData.email : ""}
 							</DataTable.Cell>
 						</DataTable.Row>
 
 						<DataTable.Row>
 							<DataTable.Cell>Phone number</DataTable.Cell>
-							<DataTable.Cell>+919769722287</DataTable.Cell>
+							<DataTable.Cell>
+								{userData.phone_no ? userData.phone_no : ""}
+							</DataTable.Cell>
 						</DataTable.Row>
 					</DataTable>
 				</View>
